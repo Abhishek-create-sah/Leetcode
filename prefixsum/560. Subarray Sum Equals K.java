cd1fc80986120bc27c7dc4f3ddb37273e23ab24d
Abhishek Kumar Sah <abhisheksah2032004@gmail.com>
2026-03-10 11:08:16 +0530

    Implemented number of subarray with sum =  k

---
+++ prefixsum/560. Subarray Sum Equals K.java
@@ -0,0 +1,19 @@
+//brute force for-loop 2 
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int n = nums.length;
+        int cnt = 0;
+        int sum = 0 ;
+        HashMap<Integer,Integer> m = new HashMap<>();
+        m.put(0,1);
+        for(int i = 0 ; i < n ; i++){
+            sum += nums[i];
+            int val = sum - k ;
+            if(m.containsKey(val)){
+                cnt += m.get(val);
+            }
+            m.put(sum,m.getOrDefault(sum,0)+1);
+        }
+        return cnt ;
+    }
+}
